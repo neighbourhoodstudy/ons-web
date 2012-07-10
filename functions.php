afb15485e5c587597839b612d107cb945a62c9fd
John Sproull <john@redmenace.org>
2012-07-10 10:06:05 -0400

    new function

--- functions.php
+++ functions.php
@@ -15,14 +15,25 @@ if ($_COOKIE['onslang'] == '') {
 function ons_display_open_header() {
 ?>
 
-<div class="loginout">
+  <div class="loginout">
+    <span><?php wp_loginout( $redirect ); ?></span>
+    <span> | </span>
+    <span><?php wp_register('',''); ?></span>
+  </div>
 
-
-<span><?php wp_loginout( $redirect ); ?></span>
-<span> | </span>
-<span><?php wp_register('',''); ?></span>
-
-</div>
+  <!-- header links -->
+  <div style="position: absolute; top: 25px;">
+    <img id="Image-Maps_7201206261231527" src="/wp-content/uploads/2012/06/transparent.png"" usemap="#Image-Maps_7201206261231527" border="0" width="1000" height="100" alt="">
+    <map id="_Image-Maps_7201206261231527" name="Image-Maps_7201206261231527">
+      <area shape="rect" coords="5,25,188,115" href="http://neighbourhoodstudy.ca/" alt="Ottawa Neighbourhood Study" title="Ottawa Neighbourhood Study">
+      <area shape="rect" coords="197,53,348,120" href="http://ottawa.ca/en/health_safety/about/oph/index.html" alt="Ottawa Public Health" title="Ottawa Public Health">
+      <area shape="rect" coords="353,53,477,120" href="http://www.uottawa.ca/" alt="University of Ottawa" title="University of Ottawa">
+      <area shape="rect" coords="478,53,622,120" href="http://www.coalitionottawa.ca" alt="Coalition of Community and Health" title="Coalition of Community and Health">
+      <area shape="rect" coords="622,53,715,120" href="http://www.ibm.com" alt="IBM" title="IBM">
+      <area shape="rect" coords="716,53,844,120" href="http://www.champlainlhin.on.ca" alt="Champlain Local Health" title="Champlain Local Health">
+      <area shape="rect" coords="847,53,991,120" href="http://www.unitedway.org" alt="United Way" title="United Way">
+    </map>
+  </div>
 
 <?php
 }
